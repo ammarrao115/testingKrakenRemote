6a399a3965b725c0a778f12639414e236eded6e9
Amr_H <ammarrao115@gmail.com>
2021-07-02 06:19:12 +0500

    added scene changer

---
+++ Assets/HomeEditing/sceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour
+{
+   public void changeScene_new(int a)
+    {
+        SceneManager.LoadScene(a);
+    }
+}
